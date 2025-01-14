590917235367779aa86395eca78b4ad8300899e4
kristophernorth <kristopher.north@gmail.com>
2025-01-14 15:05:19 -0700

    Model is set up

---
+++ lab2choosescore/Models/Chore.cs
@@ -0,0 +1,10 @@
+namespace lab2choosescore;
+
+public class Chore
+{
+  public int Id { get; set; }
+  public string Name { get; set; }
+  public string Description { get; set; }
+  public int Difficulty { get; set; }
+  public bool IsComplete { get; set; }
+}
