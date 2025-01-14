68909ef5d51cd711f71efe9ac63b9cab8cf79f58
kristophernorth <kristopher.north@gmail.com>
2025-01-14 14:44:44 -0700

    db hooked up and able to query placeholder data

--- lab2choosescore/dbSetup.sql
+++ lab2choosescore/dbSetup.sql
@@ -1,8 +1,18 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) UNIQUE COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8mb4 COMMENT '';
+CREATE TABLE chores(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  difficulty INT NOT NULL,
+  isComplete BOOLEAN DEFAULT false
+);
+
+INSERT INTO 
+chores (name, description, difficulty, isComplete)
+VALUES("Clean toilets", "Scrub dookie and hard water stains off", 2, false ),
+("Polish door handles", "Make them shine like mirrors", 3, false),
+("Vacuum carpet", "Make it nice and fresh with straight lines", 1, false),
+('Mop floors', "Make it so they're so silky smooth you can see like a mirror", 4, false);
+
+SELECT * FROM chores;
+SELECT * FROM chores WHERE id = 2;
+SELECT *, true AS done FROM chores WHERE `isComplete` != FALSE;
