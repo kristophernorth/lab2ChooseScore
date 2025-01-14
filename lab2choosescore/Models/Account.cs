239ca7ae54ca3a5cee96cd5e5cf6905284c81ce0
kristophernorth <kristopher.north@gmail.com>
2025-01-14 14:08:40 -0700

    init

---
+++ lab2choosescore/Models/Account.cs
@@ -0,0 +1,9 @@
+namespace lab2choosescore.Models;
+
+public class Account
+{
+  public string Id { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Picture { get; set; }
+}
